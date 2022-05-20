64e77113e1f127661e16bc428d02691bada4b89a
Anastasia Chernykh <anastasiacher73@gmail.com>
2022-05-20 23:28:00 +0200

    Codigo modularizado.

--- anastasia/myfile.py
+++ anastasia/myfile.py
@@ -1,68 +1,43 @@
 import random, time
 
-adiv = {"viento":"Silva sin labios, corre sin pies. En la espalda te pega pero no lo ves.",
-        "pizza":"Un cuadrado tiene de envase, redonda es su base y triangular la porción.",
-        "silencio":"Si me nombras, ya no estoy. Desaparezco.",
-        "mapa":"Hay ríos, pero no agua. Ciudades pero no casas, hay bosques pero no árboles. ¿Dónde?",
-        "reloj":"Todo el día sin parar de trabajar, y no se mueve del sitio."
+adiv = {
+        "viento" : "Silva sin labios, corre sin pies. En la espalda te pega pero no lo ves.",
+        "pizza" : "Un cuadrado tiene de envase, redonda es su base y triangular la porción.",
+        "silencio" : "Si me nombras, ya no estoy. Desaparezco.",
+        "mapa" : "Hay ríos, pero no agua. Ciudades pero no casas, hay bosques pero no árboles. ¿Dónde?",
+        "reloj" : "Todo el día sin parar de trabajar, y no se mueve del sitio."
         }
-
-print();print()
-print("Bienvenido al juego de adivinar palabras!")
-print();print()
-print("Adivina la palabra letra por letra.")
-print("Tienes 5 vidas y 1 ayuda - una letra abierta.")
-print("Al utilizar la ayuda - te bajará la puntuación, al derrotar al boss.")
-
-print();print()
-user_select=input("¿Jugamos?(si/no) -  ")
-
-vidas = 5
-lista_acertada = []
-
-if user_select == "si" or user_select == "Si" or user_select == "s":
+def welcome():
     
-    print("Pensando en la palabra secreta...")
+    print("\nBienvenido al juego de adivinar palabras!")
+    print("\nAdivina la palabra letra por letra.\n\nTienes 5 vidas y ayuda - una letra abierta.\n\nAl utilizar la ayuda - te bajarán 50 puntos, al derrotar al boss.")
+
+def generar_palabra():
+    print("\nPensando en la palabra secreta...")
     secretword = random.choice(list(adiv))
-    time.sleep(1)
-    print();print()
-    print(adiv[secretword])
     
+    time.sleep(0.5)
+    print("\n'"+adiv[secretword]+"'")
+    print("\n"," _ " *len(secretword))
     
-print(" _ " *len(secretword))
+    return secretword
+    
+def restar_vida(x):
+    
+    x-= 1
+    print("\n\nJaja! Te equivocsaste!\n\nTe quedan",x, "vidas.")
 
-print();print()
- 
-while True:
-    while vidas > 0:
-        print("Para utilizar la ayuda - marca 33")
-        letra_acertada = input("Inserta la letra: ")
+            
+def help(secretword):
         
-        if letra_acertada == "33":
-            print();print()
-            print("Por lo visto esta adivinanza está difícil para ti...")
-            print("Bueno... Te bajo la puntuación y seguimos!")
-            help = random.choice(list(secretword))
-            print();print()
-            print("La letra de ayuda, que te ha tocado es '", help,"'")
-            break
+        print("\n\nPor lo visto esta adivinanza está difícil para ti...\nBueno... Te bajo la puntuación y seguimos!")
+        
+        help_l= random.choice(list(secretword))
+        print("\nLa letra de ayuda, que te ha tocado es '", help_l,"'")
+        return help_l
 
-        if letra_acertada in secretword:
-            print();print()
-            print("Acertaste la letra")
-            lista_acertada.append(letra_acertada)
-            break
-        else:
-            vidas -= 1
-            print();print()
-            print("Jaja! Te equivocaste!")
-            print();print()
-            print("Te quedan", vidas,"vidas.")
-            break
-        
-    cont = ""
-    letras_faltan = 0
-    
+def agrupar_letras(secretword,lista_acertada,cont,letras_faltan):
+     
     for i in secretword:
         if i in lista_acertada:
             cont += i
@@ -71,11 +46,59 @@ while True:
             letras_faltan += 1
             
     print(cont)
-    
-    if vidas == 0:
-        print("Has perdido. La palabra era: ", secretword)
-        break
-            
     if letras_faltan == 0:
         print("Ganaste!")
-        break
+    
+    return cont
+            
+      
+            
+def main():
+    
+    vidas = 5
+    lista_acertada = []
+    cont = ""
+    letras_faltan = 0            
+    
+    welcome()
+    
+    u = input("\n¿Jugamos?(si/no) -  ")
+    u = u.lower()
+    
+    while True:
+        if u == "si" or u == "s":
+            secretword = generar_palabra()
+            break
+            
+        elif u == "no" or u == "n":
+            print("\nYa veo... Tienes miedo! Mejor...")
+            return 99
+        
+        else:
+            print("\nCarecter equivocado.\nIntenta de nuevo")
+                    
+    x = 0
+    while vidas > 0 and secretword != x:
+        letra = input("\nIntroduce letra o 33 para ayuda: ")
+        letra = letra.lower()
+        
+        if letra == "33":
+            help(secretword)
+            
+        elif letra not in secretword:
+            vidas -=1
+            print("\nJaja! Te equivocsaste!\n\nTe quedan",vidas, "vidas.")
+        
+        elif letra in secretword:
+            print();print()
+            print("Acertaste la letra")
+            
+            lista_acertada.append(letra)
+            
+            x = agrupar_letras(secretword,lista_acertada,cont,letras_faltan)
+
+    if vidas > 0:
+        print("\nGanaste!")
+    else:
+        print("\nHas perdido. La palabra era: ", secretword)
+main()
