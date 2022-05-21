04bace2510bd161902a4add6ec9b3d0a058fd1fd
Anastasia Chernykh <anastasiacher73@gmail.com>
2022-05-21 15:46:08 +0200

    añadiendo la voz del personaje

--- anastasia/instanciaciones.py
+++ anastasia/instanciaciones.py
@@ -1,3 +1,43 @@
+import clases as cl
+import sonidos_pantallas as sp
+import utilidades as ut
+import random,time
+        # Dimension Planet Express
+
+    #jugadores
+#bender
+bender = cl.Jugador("Bender",100,40,50,50,20,"Este jugador es muy sociopatico e impredecible. ")
+
+#habilidades
+calentador = cl.Habilidades("Calentador de agua","veneno",(5,10),"echa agua hirrviendo al villado, dejando quemaduras, que quitarán vida. ")
+estiron = cl.Habilidades("Estirón brusco","normal",(10,20),"las piernas y brazon de estiran a gran longitud bruscamente para hacer el golpe inesperado.")
+canyon = cl.Habilidades("Cañon","normal",(0,50),"Bender puede utilizarse como cañon, para dispara, lo malo es que no sabe apuntar bien, asi que puede quitar mucha o nada de vida.")
+
+#items
+#sk
+doblador = cl.Items("Doblar objetos","sk",0,"Bender fue creado como doblador de objetos profesional. Dale golpes fuertes al enemigo!",1)
+
+#ataque
+thompson = cl.Items("Thompson 1928","a",20,"subfusil favorito, desde que Bender trabajó con la mafia. Aumenta tu fuerza!",2)
+
+#sanacion
+benderbrau = cl.Items("La cerveza 'Bendërbrau'","s",15,"con la cerveza favorita de Bender aumenta tu salud!",1)
+
+#item de precision 
+detector = cl.Items("Detector de homosexuales","p",25,"con este detector de homosexuales Bender se vuelve más activo y tu precisión aumenta",1)
+
+faro = cl.Items("Faro incorpordo","p",30,"con el faro encendidio aumentas tu precision a la hora del golpe.")
+
+#villanos( Nombre, Vida, Ataque, Defensa, Precision, Velocidad)
+zapp_branningan = cl.Mob("Zapp Branningen",100,5,5,50,100)
+hipnosapo = cl.Mob("Hipnosapo",100,5,5,50,10)
+babosa_cerebral = cl.Mob("Babosa Cerebral",100,5,5,50,10)
+
+#dimension
+
+dimension_express = cl.Dimension("Planet Express",[doblador,thompson,benderbrau,detector,faro],[zapp_branningan,hipnosapo,babosa_cerebral])
+
+
 def mostrar_bender():
     print("""
 ---------------------------------------------------------------------------------------------
@@ -18,30 +58,6 @@ def mostrar_bender():
                                                                         |     """"""       |
                                                                         '-._ _   __.-'
 ---------------------------------------------------------------------------------------------
-          """)
-    
+          """.format(bender.nombre, bender.ataque,bender.defensa,bender.precision,bender.velocidad))
+        
 mostrar_bender()
-
-#items
-
-x = cl.Items()
-
-#item de precision 
-detector = cl.Items("Detector de homosexuales","p",25,"con este detector de homosexuales Bender se vuelve más activo tu tu precisión aumenta",1)
-#sk
-doblador = cl.Items("Doblar objetos","sk",0,"Bender fue creado como doblador de objetos profesional. Dale golpes fuertes al enemigo!",1)
-#ataque
-
-#sanacion 2
-
-#precision 1
-
-#villanos
-
-zapp_branningan = ()
-hipnosapo = ()
-babosa_cerebral = ()
-
-#dimension
-
-jugador = jugador
--- anastasia/myfile.py
+++ anastasia/myfile.py
@@ -1,104 +1,106 @@
 import random, time
+def anastasia_game():
+    adiv = {
+            "viento" : "Silva sin labios, corre sin pies. En la espalda te pega pero no lo ves.",
+            "pizza" : "Un cuadrado tiene de envase, redonda es su base y triangular la porción.",
+            "silencio" : "Si me nombras, ya no estoy. Desaparezco.",
+            "mapa" : "Hay ríos, pero no agua. Ciudades pero no casas, hay bosques pero no árboles. ¿Dónde?",
+            "reloj" : "Todo el día sin parar de trabajar, y no se mueve del sitio."
+            }
+    def welcome():
+        
+        print("\nBienvenido al juego de adivinar palabras!")
+        print("\nAdivina la palabra letra por letra.\n\nTienes 5 vidas y ayuda - una letra abierta.\n\nAl utilizar la ayuda - te bajarán 50 puntos, al derrotar al boss.")
 
-adiv = {
-        "viento" : "Silva sin labios, corre sin pies. En la espalda te pega pero no lo ves.",
-        "pizza" : "Un cuadrado tiene de envase, redonda es su base y triangular la porción.",
-        "silencio" : "Si me nombras, ya no estoy. Desaparezco.",
-        "mapa" : "Hay ríos, pero no agua. Ciudades pero no casas, hay bosques pero no árboles. ¿Dónde?",
-        "reloj" : "Todo el día sin parar de trabajar, y no se mueve del sitio."
-        }
-def welcome():
-    
-    print("\nBienvenido al juego de adivinar palabras!")
-    print("\nAdivina la palabra letra por letra.\n\nTienes 5 vidas y ayuda - una letra abierta.\n\nAl utilizar la ayuda - te bajarán 50 puntos, al derrotar al boss.")
+    def generar_palabra():
+        print("\nPensando en la palabra secreta...")
+        secretword = random.choice(list(adiv))
+        
+        time.sleep(0.5)
+        print("\n'"+adiv[secretword]+"'")
+        print("\n"," _ " *len(secretword))
+        
+        return secretword
+        
+    def restar_vida(x):
+        
+        x-= 1
+        print("\n\nJaja! Te equivocsaste!\n\nTe quedan",x, "vidas.")
 
-def generar_palabra():
-    print("\nPensando en la palabra secreta...")
-    secretword = random.choice(list(adiv))
-    
-    time.sleep(0.5)
-    print("\n'"+adiv[secretword]+"'")
-    print("\n"," _ " *len(secretword))
-    
-    return secretword
-    
-def restar_vida(x):
-    
-    x-= 1
-    print("\n\nJaja! Te equivocsaste!\n\nTe quedan",x, "vidas.")
-
+                
+    def help(secretword):
             
-def help(secretword):
-        
-        print("\n\nPor lo visto esta adivinanza está difícil para ti...\nBueno... Te bajo la puntuación y seguimos!")
-        
-        help_l= random.choice(list(secretword))
-        print("\nLa letra de ayuda, que te ha tocado es '", help_l,"'")
-        return help_l
+            print("\n\nPor lo visto esta adivinanza está difícil para ti...\nBueno... Te bajo la puntuación y seguimos!")
+            
+            help_l= random.choice(list(secretword))
+            print("\nLa letra de ayuda, que te ha tocado es '", help_l,"'")
+            return help_l
 
-def agrupar_letras(secretword,lista_acertada,cont,letras_faltan):
-     
-    for i in secretword:
-        if i in lista_acertada:
-            cont += i
+    def agrupar_letras(secretword,lista_acertada,cont,letras_faltan):
+        
+        for i in secretword:
+            if i in lista_acertada:
+                cont += i
+            else:
+                cont += " _ "
+                letras_faltan += 1
+                
+        print(cont)
+        if letras_faltan == 0:
+            print("Ganaste!")
+        
+        return cont
+                
+        
+                
+    def main():
+        
+        vidas = 5
+        lista_acertada = []
+        cont = ""
+        letras_faltan = 0            
+        
+        welcome()
+        
+        u = input("\n¿Jugamos?(si/no) -  ")
+        u = u.lower()
+        
+        while True:
+            if u == "si" or u == "s":
+                secretword = generar_palabra()
+                break
+                
+            elif u == "no" or u == "n":
+                print("\nYa veo... Tienes miedo! Mejor...")
+                return 99
+            
+            else:
+                print("\nCarecter equivocado.\nIntenta de nuevo")
+                        
+        x = 0
+        while vidas > 0 and secretword != x:
+            letra = input("\nIntroduce letra o 33 para ayuda: ")
+            letra = letra.lower()
+            
+            if letra == "33":
+                help(secretword)
+                
+            elif letra not in secretword:
+                vidas -=1
+                print("\nJaja! Te equivocsaste!\n\nTe quedan",vidas, "vidas.")
+            
+            elif letra in secretword:
+                print();print()
+                print("Acertaste la letra")
+                
+                lista_acertada.append(letra)
+                
+                x = agrupar_letras(secretword,lista_acertada,cont,letras_faltan)
+
+        if vidas > 0:
+            print("\nGanaste!")
         else:
-            cont += " _ "
-            letras_faltan += 1
-            
-    print(cont)
-    if letras_faltan == 0:
-        print("Ganaste!")
+            print("\nHas perdido. La palabra era: ", secretword)
+    main()
     
-    return cont
-            
-      
-            
-def main():
-    
-    vidas = 5
-    lista_acertada = []
-    cont = ""
-    letras_faltan = 0            
-    
-    welcome()
-    
-    u = input("\n¿Jugamos?(si/no) -  ")
-    u = u.lower()
-    
-    while True:
-        if u == "si" or u == "s":
-            secretword = generar_palabra()
-            break
-            
-        elif u == "no" or u == "n":
-            print("\nYa veo... Tienes miedo! Mejor...")
-            return 99
-        
-        else:
-            print("\nCarecter equivocado.\nIntenta de nuevo")
-                    
-    x = 0
-    while vidas > 0 and secretword != x:
-        letra = input("\nIntroduce letra o 33 para ayuda: ")
-        letra = letra.lower()
-        
-        if letra == "33":
-            help(secretword)
-            
-        elif letra not in secretword:
-            vidas -=1
-            print("\nJaja! Te equivocsaste!\n\nTe quedan",vidas, "vidas.")
-        
-        elif letra in secretword:
-            print();print()
-            print("Acertaste la letra")
-            
-            lista_acertada.append(letra)
-            
-            x = agrupar_letras(secretword,lista_acertada,cont,letras_faltan)
-
-    if vidas > 0:
-        print("\nGanaste!")
-    else:
-        print("\nHas perdido. La palabra era: ", secretword)
-main()
+anastasia_game()
